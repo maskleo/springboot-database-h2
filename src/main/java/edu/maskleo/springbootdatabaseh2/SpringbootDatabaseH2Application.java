2cf697d48c1f5e24850b5e7d41d350ff4359e70a
maskleo <masque@aliyun.com>
2018-05-31 16:09:38 +0800

    Initial commit

---
+++ src/main/java/edu/maskleo/springbootdatabaseh2/SpringbootDatabaseH2Application.java
@@ -0,0 +1,12 @@
+package edu.maskleo.springbootdatabaseh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDatabaseH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDatabaseH2Application.class, args);
+	}
+}
